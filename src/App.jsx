6ce6bd22c5f1f5f5ca7018ffd87385d5f59fc920
yeisonmarroquin <andresmarroquin887@gmail.com>
2023-04-17 20:27:03 -0500

    Entregable3

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import useFetch from './Hooks/useFetch'
 import getRandomlocation from './utils/getRandomlocation'
 import MainContent from './components/MainContent'
+import Loading from './components/Loading'
 
 function App() {
   
@@ -22,32 +23,40 @@ function App() {
 
   return (
     <div className="App">
-      <section className='App_inicial'>
-      <nav className='App_img'>
-      <img className='App_image' src="./image/riky.png" alt="" />
-      <img className='App_name' src="./image/name.png" alt="" />
+      {
+        location
+        ?<div>
+        <section className='App_inicial'>
+        <nav className='App_img'>
+        <img className='App_image' src="./image/riky.png" alt="" />
+        <img className='App_name' src="./image/name.png" alt="" />
+        <br />
+        <br />
       <br />
-      <br />
-    <br />
-      <h1 className='App_phrase' >Wubba Lubba Dub Dub!</h1>
-      </nav>
-      <nav>
-      <form className='App_form' onSubmit={handelSubmit}>
-          <input className='App_input' ref={inputLocation} type="number" />
-          <button className='App_btn'>Search</button>
-        </form>
-      </nav>
-      </section>
-    
+        <h1 className='App_phrase' >Wubba Lubba Dub Dub!</h1>
+        </nav>
+        <nav>
+        <form className='App_form' onSubmit={handelSubmit}>
+            <input className='App_input' ref={inputLocation} type="number" />
+            <button className='App_btn'>Search</button>
+          </form>
+        </nav>
+        </section>
       
-
-        {
-          hasError
-          ? <h1 className='App_error'>❌You can only enter a number from 1 to 120🥲</h1>
-          :  <MainContent location={location}/>
-        } 
-
-       <h1>e</h1>
+        
+  
+          {
+            hasError
+            ? <h1 className='App_error'>❌You can only enter a number from 1 to 120🥲</h1>
+            :  <MainContent location={location}/>
+          } 
+  
+         <h1>e</h1>
+        </div>
+        : <Loading/>
+      }
+    
+    
      
       
    
---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Style/loading.css'
+const Loading = () => {
+  return (
+    <div className='loading'>
+        <img src="./image/loading.gif" alt="" />
+        <img src="./image/Loading2.gif" alt="" />
+       <h1>eww</h1>
+    </div>
+  )
+}
+
+export default Loading
